4a8978df768326ebeef69ba554fc9e42b6f25d60
Kalle M. Aagaard <git@k-moeller.dk>
2026-08-24 11:17:58 +0200

    Version updated from 0.1.9.1 to 0.1.10

--- src/net_misc/__init__.py
+++ src/net_misc/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from net_misc.macaddress import MacAddress as MacAddress
 from net_misc.macaddress import MacFormat as MacFormat
 
-__version__ = "0.1.9.1"
+__version__ = "0.1.10"
 
 __all__ = [
     "MacAddress",
