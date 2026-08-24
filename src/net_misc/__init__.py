79de68cecdb1f086313bf81a954c45cc5a04fcec
Kalle M. Aagaard <git@k-moeller.dk>
2026-08-24 08:50:46 +0200

    devrel

--- src/net_misc/__init__.py
+++ src/net_misc/__init__.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
-from net_misc.macaddress import MacAddressBase as MacAddressBase
+from net_misc.macaddress import MacAddress as MacAddress
 from net_misc.macaddress import MacFormat as MacFormat
 
 __version__ = "0.1.4"
 
 __all__ = [
-    "MacAddressBase",
+    "MacAddress",
     "MacFormat",
     "__version__",
 ]
